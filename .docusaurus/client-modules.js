1027abb89b100ccccd199ca6f838665c5cdff043
wss-git <wssgryx@163.com>
2022-12-02 10:17:08 +0800

    remove .DS_Store and .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/mamba/serverless-cd/website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/mamba/serverless-cd/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/mamba/serverless-cd/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/mamba/serverless-cd/website/src/css/custom.css'),
-];
